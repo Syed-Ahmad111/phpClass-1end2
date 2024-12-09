2fea204f5f2865513f9679210b6392c12b88f00f
Syed-Ahmad111 <syedahmads160@gmail.com>
2024-12-09 15:59:44 +0500

    Add files via upload

---
+++ php.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+/*echo("I am madiha");
+echo("<h1>I am learning php</h1>");
+$name = "javascript";
+echo("Recently , I learned"." " .$name);
+///
+$num1 =10;
+$num2 = 5;
+echo($num1>$num2);*/
+
+/*$num1 = 20;
+$num2 = 10;
+echo($num1+$num2);
+echo($num1-$num2);
+echo($num1*$num2);
+echo($num1/$num2);*/
+
+/*$num =10 ;
+$num =40 ;
+echo($num);*/
+
+// $num1 = 10;
+// $num2 = 5;
+// echo ($num1 != $num2);
+// echo ($num1 === $num2);
+// echo ($num1 >= $num2);
+//  echo ($num1 !== $num2);
+
+
+$admin = "Ahmad";
+$pass = Ahmad123;
+$admin2 = "Ali";
+$pass2 = Ali123;
+
+// if ($admin == "Ahmad" && $pass == Ahmad123 || $admin2 == "Ali123" && $pass2 == Ali123) {
+//     echo ("Welcome to Admin");
+// } else {
+//     echo ("Login Filed");
+// }
+
+// switch($admin == "Ahmad" && $pass == Ahmad123 || $admin2 == "Ali" && $pass2 == Ali123){
+//     case true:
+//         echo("login successfull");
+//         break;
+//         default:
+//         echo("Login Denied");
+//          break;
+// }
+
+// for ($i = 1; $i < 6; $i++) {
+//         if ($i == 6) {
+//             continue;
+//         }
+//         echo ("<h1></h1>".$i ." <br>");
+//     }
+
+
+
+?>
